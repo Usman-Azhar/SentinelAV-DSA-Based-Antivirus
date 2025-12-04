3b3bb0608d87f263bf9230dfeaf868bb14f045ad
abdullahmalik2k5-cyber <abdullah.malik2k5@gmail.com>
2025-12-04 13:07:36 +0500

    Refined Hashtable cout statements

--- Deliverable_3/src/hashtable.h
+++ Deliverable_3/src/hashtable.h
@@ -1,6 +1,7 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
+#include <iostream>
 #include "linkedlist.h"
 #include "signature.h"
 
@@ -11,7 +12,8 @@ private:
     static const int TABLE_SIZE = 1000;
     LinkedList<Signature> table[TABLE_SIZE];
     int itemCount;
-
+    
+    // Hash function - converts string to index
     int hashFunction(const string& key) const {
         unsigned long hash = 5381;
         for (char c : key) {
@@ -19,16 +21,18 @@ private:
         }
         return hash % TABLE_SIZE;
     }
-
+    
 public:
-    HashTable() : itemCount(0) {}
-
+    HashTable() : itemCount(0) {
+        cout << "[HashTable] Initialized with size: " << TABLE_SIZE << endl;
+    }
+    
     void insert(const Signature& sig) {
         int index = hashFunction(sig.pattern);
         table[index].insert(sig);
         itemCount++;
     }
-
+    
     bool search(const string& pattern) const {
         int index = hashFunction(pattern);
         for (int i = 0; i < table[index].getSize(); i++) {
@@ -38,7 +42,7 @@ public:
         }
         return false;
     }
-
+    
     Signature getSignature(const string& pattern) const {
         int index = hashFunction(pattern);
         for (int i = 0; i < table[index].getSize(); i++) {
@@ -46,15 +50,33 @@ public:
                 return table[index].getAt(i);
             }
         }
-        return Signature();
+        return Signature();  // Return empty signature if not found
     }
-
-    int getSize() const {
-        return itemCount;
+    
+    int getSize() const { 
+        return itemCount; 
     }
-
-    double getLoadFactor() const {
-        return (double)itemCount / TABLE_SIZE;
+    
+    void displayStats() const {
+        cout << "[HashTable] Total items: " << itemCount << endl;
+        cout << "[HashTable] Table size: " << TABLE_SIZE << endl;
+        cout << "[HashTable] Load factor: " 
+             << (float)itemCount / TABLE_SIZE << endl;
+        
+        // Calculate collisions
+        int occupiedBuckets = 0;
+        int maxChainLength = 0;
+        for (int i = 0; i < TABLE_SIZE; i++) {
+            int bucketSize = table[i].getSize();
+            if (bucketSize > 0) {
+                occupiedBuckets++;
+                if (bucketSize > maxChainLength) {
+                    maxChainLength = bucketSize;
+                }
+            }
+        }
+        cout << "[HashTable] Occupied buckets: " << occupiedBuckets << endl;
+        cout << "[HashTable] Max chain length: " << maxChainLength << endl;
     }
 };
 
